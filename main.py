2ef579d5c4c7264fac883e2164c4f37b8b566ecd
beboptank <beboptank12@gmail.com>
2023-04-26 15:00:03 -0400

    Added blackjack rules to comments in main.py; added player.py and dealer.py to hold Player and Dealer classes

---
+++ main.py
@@ -0,0 +1,8 @@
+# Blackjack / 21 Rules:
+# - Played using cards
+# - Don't go over 21 (a bust)
+# - Cards 2 to 10 count as face value
+# - Cards J, Q, and K count as 10
+# - An Ace counts as 1 or 11
+# - If your score and the dealer's score are the same, you draw
+# - If the dealer ends up with a hand that is smaller than 17, they must draw
